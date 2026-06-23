c16f7f0db5726dead25668476a95f4ea83a20ca9
opitomiai-alt <opitomiai@gmail.com>
2026-06-23 12:08:32 +0300

    chore: export express app instead of listening in eshop-basic template for serverless compatibility

--- templates/eshop-basic/server.js
+++ templates/eshop-basic/server.js
@@ -1,4 +1,17 @@
-const express=require('express');const app=express();app.use(express.json());app.use(express.static('public'));
-const products=[{id:1,name:'Продукт 1',price:19.99},{id:2,name:'Продукт 2',price:29.99},{id:3,name:'Продукт 3',price:9.99}];
-app.get('/api/products',(req,res)=>res.json(products));
-app.listen(3000,()=>console.log('Eshop on http://localhost:3000'));
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const products = [
+  {id:1,name:'Продукт 1',price:19.99},
+  {id:2,name:'Продукт 2',price:29.99},
+  {id:3,name:'Продукт 3',price:9.99}
+];
+
+app.get('/api/products', (req,res) => res.json(products));
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+module.exports = app;
